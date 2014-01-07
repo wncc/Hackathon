b9ccb8d16a7c21ae0146cf8edcfa0fd839005025
sushant <hiraysushant@gmail.com>
2014-01-07 17:38:52 +0530

    Refactored images

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+            <p>Copyright © 2014 <a href="http://www.wncc-iitb.org/">Web and Coding Club</a>. All rights reserved. Hackathon&#8482; is a trademark of WnCC,IIT Bombay.</p>
+        <nav>
+            <ul>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="https://github.com/wncc/Hackathon" target="_blank">Source Code</a></li>
+                
+            </ul>
+        </nav>
+    </footer>
+</body>
+</html>
