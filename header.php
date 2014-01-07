b2d2610d6c237ee5b5c85858ae651a9f4e2dd9ee
sushant <hiraysushant@gmail.com>
2014-01-07 20:11:36 +0530

    Reverted back header

--- header.php
+++ header.php
@@ -12,17 +12,15 @@
 
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/about.css">
-    <link rel="stylesheet" href="css/event.css">
-    <link rel="stylesheet" href="css/style.css">
     <style type="text/css">.tk-proxima-nova{font-family:"proxima-nova",sans-serif;}</style>
     <link href="css/d.css" rel="stylesheet">
 </head>
-<body data-twttr-rendered="true" class="schedule"><div style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; padding: 0px; border: 0px none; background: none repeat scroll 0% 0% transparent; overflow: hidden; position: fixed; z-index: 16000002; display: none; width: 0px; height: 0px; right: 10px; bottom: 0px;" __jx__id="___$_51 ___$_51" class="zopim"><iframe src="javascript:false" style="background-color: transparent; vertical-align: text-bottom; position: relative; width: 100%; height: 100%; margin: 0px; overflow: hidden;" frameborder="0"></iframe></div><div style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; padding: 0px; border: 0px none; background: none repeat scroll 0% 0% transparent; overflow: hidden; position: fixed; z-index: 16000001; right: 10px; bottom: 0px; border-top-left-radius: 5px; border-top-right-radius: 5px; display: none; width: 290px; height: 400px; box-shadow: 0px 0px 3px 2px rgba(0, 0, 0, 0.1);" __jx__id="___$_4 ___$_4" class="zopim"><iframe src="javascript:false" style="background-color: transparent; vertical-align: text-bottom; position: relative; width: 100%; height: 100%; margin: 0px; overflow: hidden;" frameborder="0"></iframe></div>
-        <header>
+<body data-twttr-rendered="true" class="index">
+    <header>
         <h1><a href="index.html">Hackathon</a></h1>
         <nav>
             <ul>
-                <li class="active"><a href="index.php">Home</a></li>
+                <li class="active"><a href="/">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="resources.php">Resources and Prizes</a></li>
                 <li><a href="schedule.php">Schedule</a></li>
