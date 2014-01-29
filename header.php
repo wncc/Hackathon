82c67fdfdf4e952a72c620206ac25bbc4dd2fccc
sushant <hiraysushant@gmail.com>
2014-01-29 20:13:57 +0530

    Added favicon

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html class=""><head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
     
-    <title>Hackathon</title>
+    <title>WnCC | Hackathon</title>
     <meta name="description" content="Hackathon is a  24-hour programming marathon events for IIT Bombay students who are new to programming. All events are volunteer-run, and supported by Web and Coding club IIT Bombay.">
    
     <meta property="og:title" content="Hackathon">
@@ -10,6 +10,7 @@
     <meta property="og:type" content="website">
     <meta property="og:description" content="Hackathon is a  24-hour programming marathon events for IIT Bombay students who are new to programming. All events are volunteer-run, and supported by Web and Coding club IIT Bombay.">
 
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
     <link rel="stylesheet" href="css/global.css">
     <style type="text/css">.tk-proxima-nova{font-family:"proxima-nova",sans-serif;}</style>
     <link href="css/d.css" rel="stylesheet">
