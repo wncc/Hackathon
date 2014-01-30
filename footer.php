61e2abdcbed104bb943452a0f8cb0bf21c4dd188
sushant <hiraysushant@gmail.com>
2014-01-30 10:21:11 +0530

    Added analytics

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer>
+	<footer>
             <p>Copyright © 2014 <a href="http://www.wncc-iitb.org/">Web and Coding Club</a>. All rights reserved. Hackathon&#8482; is a trademark of WnCC,IIT Bombay.</p>
         <nav>
             <ul>
@@ -8,5 +8,15 @@
             </ul>
         </nav>
     </footer>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-40046157-4', 'wncc-iitb.org');
+      ga('send', 'pageview');
+
+  </script>
 </body>
 </html>
